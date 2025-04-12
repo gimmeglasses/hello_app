5dbcd39db1dcf228b5135a649aea7e7901fba296
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-04-12 13:48:30 +0000

    Initial submission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def hello
+    #  render html: 'hello, world!'
+    # render html: 'hola, mundo!'
+    render html: '¡Hola, mundo!'
+  end
+  def goodbye
+    render html: "goobye, world!"
+  end
 end
